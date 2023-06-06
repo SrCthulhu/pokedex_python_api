cba9cc206c6526a7458858c2d9a9f5d851046515
Mr Cthulhu <danielparucho@gmail.com>
2023-06-06 19:58:10 -0300

    Token setup, user Document response fix

--- api/app.py
+++ api/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import pymongo
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from bson import json_util
@@ -28,12 +29,8 @@ def login():
         return {
             "success": False
         }
-    token_payload = {
-        'user_id': str(userDocument['_id']),
-        'exp': datetime.utcnow() + timedelta(hours=24)
-    }
-    token = jwt.encode(
-        token_payload, env['TOKEN_SECRET_KEY'], algorithm='HS256')
+    token = userDocument['token']
+    print("Logged user:", token)
     return {
         "success": True,
         "token": str(token),
@@ -43,8 +40,6 @@ def login():
 @app.route("/registry", methods=["POST"])
 def registry():
     body_registry = request.json
-    print(body_registry)
-
     if body_registry['user'] == "" or body_registry['email'] == "" or body_registry['pass'] == "" or len(body_registry['user']) < 3 or len(body_registry['pass']) < 5:
         return {
             "success": False
@@ -72,9 +67,8 @@ def registry():
 
     # Agregamos el nuevo campo token a nuestro documento de usuario.
     body_registry['token'] = str(token)
-
     db.users.insert_one(body_registry)
-
+    print('generated token:', body_registry['token'])
     return {
         "success": True,
         "token": str(token),
@@ -83,45 +77,72 @@ def registry():
 
 @app.route("/avatars", methods=["GET"])
 def findAvatars():
+    token = request.headers.get('Token')
+    user = db.users.find_one({'token': token})
+    if user == None:
+        return {
+            "success": False,
+            "error": "invalid token",
+        }
     avatars = db.avatars.find()
+
     return {
         "success": True,
-        "avatars": json.loads(json_util.dumps(avatars))
+        "avatars": json.loads(json_util.dumps(avatars)),
+        "userDocument": json.loads(json_util.dumps(user)),
+        "Token": str(token)
     }
 
 
 @app.route("/avatarSelected", methods=["POST"])
 def avatarSelected():
     body = request.json
-    token = body.get("accessToken")
-    avatar = body.get("avatar")
-
-    # Busco el usuario usando el token
+    token = body.get("token")
+    avatar = body.get("selectedAvatar")
     user = db.users.find_one({"token": token})
 
-    if user is None:
+    # Compruebo que el token coincida
+    if str(user["token"]) != token:
         return {
             "success": False,
-            "message": "User not found",
+            "message": "Invalid token",
         }
 
-    # Agrega el avatar selecionado a la lista de avatars del usuario.
-    avatars = user.get("avatar", [])
-    avatars.append(avatar)
-
+    # Agrega el avatar selecionado al objeto usuario.
+    user["avatar"] = avatar
     # Actualiza el documento del usuario con la nueva lista de avatar
-    db.users.update_one({"_id": user["_id"]}, {"$set": {"avatar": avatars}})
-
+    db.users.update_one({"_id": user["_id"]}, {
+                        "$set": {"avatar": user["avatar"]}})
     return {
         "success": True,
         "token": str(token),
     }
 
 
+@app.route("/pokemons", methods=["GET"])
+def get_pokemons():
+    # Buscamos los elementos y organizamos en orden ascendiente por la clave "number" al contrario: DESCENDING
+    pokemons = db.pokemons.find({"main": True}).sort(
+        "number", pymongo.ASCENDING)
+    return {
+        "success": True,
+        "pokemons": json.loads(json_util.dumps(pokemons)),
+    }
+
+
+@app.route("/userPokemon", methods=["GET"])
+def get_UserPokemon():
+    userPokemon = db.pokemons.find_one({"main": False, "owner": "user_id"})
+    print(userPokemon)
+    return {
+        "success": True,
+        "userPokemon": json.loads(json_util.dumps(userPokemon)),
+    }
+
+
 @app.route("/collected", methods=["POST"])
 def saveCollected():
     body_Collected = request.json
-    print({"body_Collected": body_Collected})
     db.collection.insert_one(body_Collected)
     return {
         "success": True,
